e33109ada7f74dcbf85435fccd46605c346811e2
Aravind25100 <77260892+Aravind25100@users.noreply.github.com>
2021-01-11 18:23:56 +0530

    Add files via upload

---
+++ TimeLoop.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class TimeLoop{
+    public static void main(String[]args){
+        Scanner kbd = new Scanner(System.in);
+        int N = kbd.nextInt();
+        for(int i=0;i<N;i++)
+           System.out.println((i+1)+" Abracadabra");
+    }
+}
---
+++ timeloop.rb
@@ -0,0 +1,4 @@
+N = gets.to_i
+for i in 1..N
+  puts(i," Abracadabra")
+end
